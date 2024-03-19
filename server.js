704f6e5f4595f778d7c81a7fc96a48b06e54070b
patrickarmani <arm22001@byui.edu>
2024-03-19 08:51:10 -0300

    Suggested changes

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Controll-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, OPTIONS, DELETE"
+  );
   next();
-});
+})
+  .use(cors({ methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}))
+  .use(cors({ origin: '*'}))
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
